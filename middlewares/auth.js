efcc5257d188e80fb80b2e8b79c5535deca64487
ibrahyma <36759387+ibrahyma@users.noreply.github.com>
2020-12-01 16:24:01 +0100

    Add files via upload

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,1 +1,21 @@
+const jwt = require('jsonwebtoken');
 
+module.exports = (req, res, next) =>
+{
+    try
+    {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, "RANDOM_SECRET_KEY");
+        const userId = decodedToken.userId;
+
+        if (req.body.userId && req.body.userId !== userId)
+            throw "UserID invalide.";
+        else
+            next();
+    }
+    catch (error)
+    {
+        // console.error(error);
+        res.status(401).json({ message: error.message, error: error });
+    }
+}
