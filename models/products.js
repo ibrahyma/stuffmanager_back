0f09c0c7196739b1a4dd6392416b38e844738315
ibrahyma <36759387+ibrahyma@users.noreply.github.com>
2020-12-01 16:24:50 +0100

    Add files via upload

--- models/products.js
+++ models/products.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
 
+const productsSchema = mongoose.Schema(
+{
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    imageUrl: { type: String, required: true }
+});
+
+module.exports = mongoose.model("Products", productsSchema);
---
+++ models/users.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const usersSchema = mongoose.Schema(
+{
+    login: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true},
+    password: { type: String, required: true }
+});
+
+module.exports = mongoose.model("Users", usersSchema);
